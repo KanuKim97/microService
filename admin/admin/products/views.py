1e54ff9ebd61f64984a7c4c428d4a4bea417382c
KanuKim97 <74421057+KanuKim97@users.noreply.github.com>
2021-10-28 21:45:20 +0900

    mod django

--- admin/admin/products/views.py
+++ admin/admin/products/views.py
@@ -4,31 +4,33 @@ from rest_framework.response import Response
 from .models import Products
 from .serializers import ProductSerailizer
 
+# Part RestApi
+
 class ProductViewSet(viewsets.ViewSet):
-    def list(self, request):
+    def list(self, request):    # /api/products
         products = Products.objects.all()
         serializer = ProductSerailizer(products, many=True)
         return Response(serializer.data)
 
-    def create(self, request):
+    def create(self, request):  # /api/products
         serializer = ProductSerailizer(data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
 
-    def retrieve(self, request, pk=None):
+    def retrieve(self, request, pk=None):   # /api/products/<str:id>
         product = Products.objects.get(id=pk)
         serializer = ProductSerailizer(product)
         return Response(serializer.data)
 
-    def update(self, request, pk=None):
+    def update(self, request, pk=None):     # /api/produccts/<str:id>
         product = Products.objects.get(id=pk)
         serializer = ProductSerailizer(instance=product, data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save
         return Response(serializer.data, status=status.HTTP_202_ACCEPTED)
 
-    def destroy(self, request, pk=None):
+    def destroy(self, request, pk=None):    # /api/produccts/<str:id>
         product = Products.objects.get(id=pk)
         product.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
